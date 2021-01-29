d94a24605bc5857a9fbe01a08f57902c8c48ee46
Simon Davis <simon@breathingdust.com>
2021-01-29 12:43:06 -0800

    Update push-changes.sh

--- scripts/push-changes.sh
+++ scripts/push-changes.sh
@@ -11,7 +11,7 @@ if [[ `git status --porcelain` ]]; then
     git config --local user.email $1
     git config --local user.name $2
     git add CHANGELOG.md
-    git commit -m $3 
+    git commit -m "$3" 
     git push
 else
     echo "No changes to push"
